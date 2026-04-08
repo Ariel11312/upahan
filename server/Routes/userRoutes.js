33f623ab57d7606bebb4f49c7511f7a89eeee194
Ariel11312 <ariellabuson11@gmail.com>
2026-04-09 06:56:21 +0800

    sever1 update 040920260650AM

--- server/Routes/userRoutes.js
+++ server/Routes/userRoutes.js
@@ -1,11 +1,15 @@
 import { Router } from 'express'
 import { createUser, getUserById, loginUser } from '../Controllers/userController.js'
 import authMiddleware from '../Middleware/authMiddleware.js'
+import {
+  loginIpLimiter,
+  signupLimiter,
+} from '../Auth/userAuth.js'
 
 const router = Router()
 
-router.get('/', authMiddleware, getUserById)
-router.post('/signup', createUser)
-router.post('/login', loginUser)
+router.get('/',        authMiddleware,  getUserById)
+router.post('/signup', signupLimiter,   createUser)
+router.post('/login',  loginIpLimiter,  loginUser)
 
 export default router
